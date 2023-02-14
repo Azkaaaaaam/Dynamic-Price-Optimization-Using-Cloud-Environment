f976865e62f2a97f26165680803a1bf3af331c4e
Azkaaaaaam <124452795+Azkaaaaaam@users.noreply.github.com>
2023-02-14 18:06:14 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,46 +1,186 @@
-import streamlit as st
-import pandas as pd
+# -*- coding: utf-8 -*-
+# Copyright 2018-2022 Streamlit Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""An example of showing geographic data."""
+
+import altair as alt
 import numpy as np
-import joblib
+import pandas as pd
+import pydeck as pdk
+import streamlit as st
 
-# Load the machine learning model and preprocessing objects
-model = joblib.load('model.pkl')
-scaler = joblib.load('scaler.pkl')
-onehot = joblib.load('onehot.pkl')
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="NYC Ridesharing Demo", page_icon=":taxi:")
 
-# Define the Streamlit app
-def app():
-    # Define the app title
-    st.title('NYC Taxi Fare Prediction App')
-    
-    # Define the input fields
-    pickup_datetime = st.date_input("Pickup Date")
-    pickup_time = st.time_input("Pickup Time")
-    pickup_longitude = st.number_input("Pickup Longitude")
-    pickup_latitude = st.number_input("Pickup Latitude")
-    dropoff_longitude = st.number_input("Dropoff Longitude")
-    dropoff_latitude = st.number_input("Dropoff Latitude")
-    passenger_count = st.number_input("Passenger Count", min_value=1, max_value=6, value=1)
-    
-    # Prepare the input data
-    input_data = pd.DataFrame({
-        'pickup_datetime': [pd.to_datetime(str(pickup_datetime) + ' ' + str(pickup_time))],
-        'pickup_longitude': [pickup_longitude],
-        'pickup_latitude': [pickup_latitude],
-        'dropoff_longitude': [dropoff_longitude],
-        'dropoff_latitude': [dropoff_latitude],
-        'passenger_count': [passenger_count]
-    })
-    
-    # Preprocess the input data
-    input_data['hour'] = input_data['pickup_datetime'].dt.hour
-    input_data['weekday'] = input_data['pickup_datetime'].dt.weekday
-    input_data = scaler.transform(input_data)
-    input_data = onehot.transform(input_data)
-    
-    # Make the prediction
-    prediction = model.predict(input_data)
-    
-    # Display the prediction
-    st.header("Estimated Taxi Fare:")
-    st.write("$" + str(round(prediction[0], 2)))
+# LOAD DATA ONCE
+@st.experimental_singleton
+def load_data():
+    data = pd.read_csv(
+        "uber-raw-data-sep14.csv.gz",
+        nrows=100000,  # approx. 10% of data
+        names=[
+            "date/time",
+            "lat",
+            "lon",
+        ],  # specify names directly since they don't change
+        skiprows=1,  # don't read header since names specified directly
+        usecols=[0, 1, 2],  # doesn't load last column, constant value "B02512"
+        parse_dates=[
+            "date/time"
+        ],  # set as datetime instead of converting after the fact
+    )
+
+    return data
+
+
+# FUNCTION FOR AIRPORT MAPS
+def map(data, lat, lon, zoom):
+    st.write(
+        pdk.Deck(
+            map_style="mapbox://styles/mapbox/light-v9",
+            initial_view_state={
+                "latitude": lat,
+                "longitude": lon,
+                "zoom": zoom,
+                "pitch": 50,
+            },
+            layers=[
+                pdk.Layer(
+                    "HexagonLayer",
+                    data=data,
+                    get_position=["lon", "lat"],
+                    radius=100,
+                    elevation_scale=4,
+                    elevation_range=[0, 1000],
+                    pickable=True,
+                    extruded=True,
+                ),
+            ],
+        )
+    )
+
+
+# FILTER DATA FOR A SPECIFIC HOUR, CACHE
+@st.experimental_memo
+def filterdata(df, hour_selected):
+    return df[df["date/time"].dt.hour == hour_selected]
+
+
+# CALCULATE MIDPOINT FOR GIVEN SET OF DATA
+@st.experimental_memo
+def mpoint(lat, lon):
+    return (np.average(lat), np.average(lon))
+
+
+# FILTER DATA BY HOUR
+@st.experimental_memo
+def histdata(df, hr):
+    filtered = data[
+        (df["date/time"].dt.hour >= hr) & (df["date/time"].dt.hour < (hr + 1))
+    ]
+
+    hist = np.histogram(filtered["date/time"].dt.minute, bins=60, range=(0, 60))[0]
+
+    return pd.DataFrame({"minute": range(60), "pickups": hist})
+
+
+# STREAMLIT APP LAYOUT
+data = load_data()
+
+# LAYING OUT THE TOP SECTION OF THE APP
+row1_1, row1_2 = st.columns((2, 3))
+
+# SEE IF THERE'S A QUERY PARAM IN THE URL (e.g. ?pickup_hour=2)
+# THIS ALLOWS YOU TO PASS A STATEFUL URL TO SOMEONE WITH A SPECIFIC HOUR SELECTED,
+# E.G. https://share.streamlit.io/streamlit/demo-uber-nyc-pickups/main?pickup_hour=2
+if not st.session_state.get("url_synced", False):
+    try:
+        pickup_hour = int(st.experimental_get_query_params()["pickup_hour"][0])
+        st.session_state["pickup_hour"] = pickup_hour
+        st.session_state["url_synced"] = True
+    except KeyError:
+        pass
+
+# IF THE SLIDER CHANGES, UPDATE THE QUERY PARAM
+def update_query_params():
+    hour_selected = st.session_state["pickup_hour"]
+    st.experimental_set_query_params(pickup_hour=hour_selected)
+
+
+with row1_1:
+    st.title("NYC Uber Ridesharing Data")
+    hour_selected = st.slider(
+        "Select hour of pickup", 0, 23, key="pickup_hour", on_change=update_query_params
+    )
+
+
+with row1_2:
+    st.write(
+        """
+    ##
+    Examining how Uber pickups vary over time in New York City's and at its major regional airports.
+    By sliding the slider on the left you can view different slices of time and explore different transportation trends.
+    """
+    )
+
+# LAYING OUT THE MIDDLE SECTION OF THE APP WITH THE MAPS
+row2_1, row2_2, row2_3, row2_4 = st.columns((2, 1, 1, 1))
+
+# SETTING THE ZOOM LOCATIONS FOR THE AIRPORTS
+la_guardia = [40.7900, -73.8700]
+jfk = [40.6650, -73.7821]
+newark = [40.7090, -74.1805]
+zoom_level = 12
+midpoint = mpoint(data["lat"], data["lon"])
+
+with row2_1:
+    st.write(
+        f"""**All New York City from {hour_selected}:00 and {(hour_selected + 1) % 24}:00**"""
+    )
+    map(filterdata(data, hour_selected), midpoint[0], midpoint[1], 11)
+
+with row2_2:
+    st.write("**La Guardia Airport**")
+    map(filterdata(data, hour_selected), la_guardia[0], la_guardia[1], zoom_level)
+
+with row2_3:
+    st.write("**JFK Airport**")
+    map(filterdata(data, hour_selected), jfk[0], jfk[1], zoom_level)
+
+with row2_4:
+    st.write("**Newark Airport**")
+    map(filterdata(data, hour_selected), newark[0], newark[1], zoom_level)
+
+# CALCULATING DATA FOR THE HISTOGRAM
+chart_data = histdata(data, hour_selected)
+
+# LAYING OUT THE HISTOGRAM SECTION
+st.write(
+    f"""**Breakdown of rides per minute between {hour_selected}:00 and {(hour_selected + 1) % 24}:00**"""
+)
+
+st.altair_chart(
+    alt.Chart(chart_data)
+    .mark_area(
+        interpolate="step-after",
+    )
+    .encode(
+        x=alt.X("minute:Q", scale=alt.Scale(nice=False)),
+        y=alt.Y("pickups:Q"),
+        tooltip=["minute", "pickups"],
+    )
+    .configure_mark(opacity=0.2, color="red"),
+    use_container_width=True,
+)
