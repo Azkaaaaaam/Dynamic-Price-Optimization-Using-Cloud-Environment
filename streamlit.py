e9ea60c53ee62909c6e4d879e6a3a0803166995f
Azkaaaaaam <124452795+Azkaaaaaam@users.noreply.github.com>
2023-02-16 11:19:10 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -17,19 +17,23 @@ def load_data(nrows):
 
 data_load_state = st.text('Loading data...')
 data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
+data_load_state.text("Done!")
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+col1, col2 = st.columns(2)
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+with col1:
+            if st.checkbox('Show raw data'):
+                st.subheader('Raw data')
+                st.write(data)
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+            st.subheader('Number of pickups by hour')
+            hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+            st.bar_chart(hist_values)
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+            # Some number in the range 0-23
+            hour_to_filter = st.slider('hour', 0, 23, 17)
+            filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+with col2:
+            st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+            st.map(filtered_data)
