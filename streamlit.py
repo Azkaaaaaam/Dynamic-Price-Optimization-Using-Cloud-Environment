b54160cff6a6aef300b3a13c7d569427ec101181
Azkaaaaaam <124452795+Azkaaaaaam@users.noreply.github.com>
2023-02-16 15:45:52 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -55,34 +55,35 @@ df = pd.DataFrame(data_dict)
 col1, col2 = st.columns(2)
 
 with col1:
-            with st.form("my_form"):
-                st.selectbox(
-                    'Pickup location',
-                    df["Pickup location"].unique()
-                )  
-                st.selectbox(
-                    'Dropoff location',
-                    df["Dropoff location"].unique()
-                )  
-                st.date_input(
-                    'Date of pickup',
-                    dt.date(2023, 5, 25)
-                )            
-                st.time_input(
-                    'Time of pickup',
-                    dt.time(0, 0),
-                    help='Specify the time of pickup'
-                )
-                st.number_input(
-                    'Number of Users',
-                    value=1,
-                    min_value=1,
-                    max_value=5,
-                    help='The number of users should be between 1 and 5'
-                )
+    with st.form("my_form"):
+        pickup_location = st.selectbox(
+            'Pickup location',
+            df["Pickup location"].unique()
+        )  
+        dropoff_location = st.selectbox(
+            'Dropoff location',
+            df["Dropoff location"].unique()
+        )  
+        pickup_date = st.date_input(
+            'Date of pickup',
+            dt.date(2023, 5, 25)
+        )            
+        pickup_time = st.time_input(
+            'Time of pickup',
+            dt.time(0, 0),
+            help='Specify the time of pickup'
+        )
+        num_users = st.number_input(
+            'Number of Users',
+            value=1,
+            min_value=1,
+            max_value=5,
+            help='The number of users should be between 1 and 5'
+        )
 
-                # Every form must have a submit button.
-                submitted = st.form_submit_button("Submit")
+        # Every form must have a submit button.
+        submitted = st.form_submit_button("Submit")
+
 if submitted:
     # Access the form results if the form was submitted
     pulocation = pickup_location
