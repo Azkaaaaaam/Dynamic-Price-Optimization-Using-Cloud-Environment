257b3b81773b9eb22b0f3a0147db3fd517458f87
Azkaaaaaam <124452795+Azkaaaaaam@users.noreply.github.com>
2023-02-16 12:38:47 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -24,33 +24,33 @@ data_load_state.text("Done!")
 col1, col2 = st.columns(2)
 
 with col1:
-
             with st.form("my_form"):
-                        st.selectbox(
-                          'Pickup location',
-                          ('NYC','NYC2','NYC4','NYC5'),
-                        )  
-                        st.selectbox(
-                          'Dropoff location',
-                          ('NYC','NYC2','NYC4','NYC5'),
-                        )  
-                        st.date_input(
-                          'Date of pickup',
-                          dt.date(2023, 25, 5)
-                        )            
-                        st.time_input(
-                          'Label goes here',
-                          dt.time(0, 0),
-                          help='Help message goes here'
-                        )
-                        st.number_input(
-                          'Number of Users',
-                          1,
-                          help='The number of users need to be from 1 to 5'
-                        )
-                             # Every form must have a submit button.
-            submitted = st.form_submit_button("Estimate my Fare")
-
+                st.selectbox(
+                    'Pickup location',
+                    ('NYC','NYC2','NYC4','NYC5')
+                )  
+                st.selectbox(
+                    'Dropoff location',
+                    ('NYC','NYC2','NYC4','NYC5')
+                )  
+                st.date_input(
+                    'Date of pickup',
+                    dt.date(2023, 5, 25)
+                )            
+                st.time_input(
+                    'Time of pickup',
+                    dt.time(0, 0),
+                    help='Specify the time of pickup'
+                )
+                st.number_input(
+                    'Number of Users',
+                    1,
+                    min_value=1,
+                    max_value=5,
+                    help='The number of users should be between 1 and 5'
+                )
+                # Every form must have a submit button.
+                submitted = st.form_submit_button("Estimate my Fare")
 with col2:
             chart_data = pd.DataFrame(
                np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
