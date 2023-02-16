8ca4841833594763a5a3cc9e13369ae12dcd2668
Azkaaaaaam <124452795+Azkaaaaaam@users.noreply.github.com>
2023-02-16 13:29:02 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -12,8 +12,20 @@ st.title('Uber pickups in NYC')
 url = "https://data.cityofnewyork.us/api/views/755u-8jsi/rows.xml?accessType=DOWNLOAD"
 response = urllib.request.urlopen(url)
 xml_data = response.read()
-print(xml_data)
 
+# Parse the XML data
+root = ET.fromstring(xml_data)
+
+# Extract the data into a list of lists for the CSV data
+csv_data = [['location_id', 'borough', 'zone', 'service_zone']]
+for elem in root.iter("row"):
+    location_id = elem.find("location_id").text
+    borough = elem.find("borough").text
+    zone = elem.find("zone").text
+    service_zone = elem.find("service_zone").text
+    csv_data.append([location_id, borough, zone, service_zone])
+
+print(csv_data)
 
 
 col1, col2 = st.columns(2)
