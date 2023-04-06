6be8c9acd728a1913c96d4c5806e230161e0c2f2
Azkaaaaaam <124452795+Azkaaaaaam@users.noreply.github.com>
2023-04-06 22:07:15 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -26,7 +26,8 @@ model = joblib.load('model.pkl')
 cols = ['trip_distance', 'dolocationid','month', 'temp', 'feelslike', 'snow', 'windspeed', 'cloudcover', 'Day', 'Hour', 'Weekday', 'duration']
 final_features=np.array([[1, 1, 0, 1.495619524, 2.704968711, 15.95906133, 3.5, 0.5, 0, 25.2, 37.9, 36.94705882]])
 prediction = model.predict(final_features)
-print(prediction)
+st.write(prediction)
+
 url2 = "https://data.cityofnewyork.us/resource/m6nq-qud6.json?$query=SELECT%0A%20%20%60tpep_pickup_datetime%60%2C%0A%20%20%60tpep_dropoff_datetime%60%2C%0A%20%20%60passenger_count%60%2C%0A%20%20%60trip_distance%60%2C%0A%20%20%60pulocationid%60%2C%0A%20%20%60dolocationid%60%2C%0A%20%20%60total_amount%60"
 
 response2 = requests.get(url2)
