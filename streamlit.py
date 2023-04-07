85f9d8949a88c0bfe4e84beb429cac2257040232
Azkaaaaaam <124452795+Azkaaaaaam@users.noreply.github.com>
2023-04-07 11:00:52 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -215,8 +215,8 @@ with col3:
           model_path = os.path.join(os.path.dirname(__file__), 'modelprice.pkl')
           #model = pickle.load(open(model_path, 'rb'))
           model2 = joblib.load('modelprice.pkl')
-          #cols2 = ['trip_distance', 'dolocationid','month', 'temp', 'feelslike', 'snow', 'windspeed', 'cloudcover', 'Day', 'Hour', 'Weekday', 'duration']
-          final_features2=np.array([[distance,43,user_month,matching_data['temp'], matching_data['feelslike'], matching_data['snow'], matching_data['windspeed'], matching_data['cloudcover'],user_day, user_hour,2,duration]])
+          cols2 = ['trip_distance', 'dolocationid','total_amount','month', 'temp', 'feelslike', 'snow', 'windspeed', 'cloudcover', 'Day', 'Hour', 'Weekday', 'duration']
+          final_features2=np.array([[distance,43,0,user_month,matching_data['temp'], matching_data['feelslike'], matching_data['snow'], matching_data['windspeed'], matching_data['cloudcover'],user_day, user_hour,2,duration]])
       #    final_features2=np.array([[2.5, 35, 5, 20.0, 18.5, 0, 5.0, 60, 7, 14, 2, 600]])
           prediction2 = model2.predict(final_features2)
           st.write(prediction2)
