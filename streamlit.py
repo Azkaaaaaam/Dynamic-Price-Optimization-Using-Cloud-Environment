8a07014ea6583c7ed093eeb620cc712b71b84b6d
Azkaaaaaam <124452795+Azkaaaaaam@users.noreply.github.com>
2023-04-10 18:14:06 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -14,6 +14,7 @@ import pickle
 import joblib
 import altair as alt
 from PIL import Image
+import streamlit_fontawesome as stf
 
 ############################################################################# Layout
 
